f5c7e298aec178a5a74846cb6e42019d17944a1a
johni1228 <akirus1228@gmail.com>
2021-11-19 20:14:26 +0900

    fea: Init

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,126 @@
+import { noop } from "lodash";
+import { DropdownMenuItems, DropdownMenuItemType } from "../../components/DropdownMenu/types";
+import { MenuItemsType } from "../../components/MenuItems/types";
+import { LinkStatus } from "./types";
+
+export const status = {
+  LIVE: <LinkStatus>{
+    text: "LIVE",
+    color: "failure",
+  },
+  SOON: <LinkStatus>{
+    text: "SOON",
+    color: "warning",
+  },
+  NEW: <LinkStatus>{
+    text: "NEW",
+    color: "success",
+  },
+};
+
+export const links: MenuItemsType[] = [
+  
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Farms",
+    href: "/",
+  },
+  {
+    label: "Nest",
+    href: "/",
+  },
+  {
+    label: "Pools",
+    href: "/",
+  },
+  {
+    label: "",
+    href: "/",
+    icon: "More",
+    items: [
+      {
+        label: "Info & Analytics",
+        href: "/",
+      },
+      {
+        label: "IFO Token Sales",
+        href: "/",
+        status: status.SOON,
+      },
+      {
+        type: DropdownMenuItemType.DIVIDER,
+      },
+      {
+        label: "NFT Collectibles",
+        href: "/",
+      },
+      {
+        label: "Team Leaderboard",
+        href: "/",
+      },
+      {
+        type: DropdownMenuItemType.DIVIDER,
+      },
+      {
+        label: "Blog",
+        href: "/",
+      },
+      {
+        label: "Docs & Guides",
+        href: "/",
+        type: DropdownMenuItemType.EXTERNAL_LINK,
+      },
+    ],
+  },
+];
+
+export const userMenulinks: DropdownMenuItems[] = [
+  {
+    label: "Wallet",
+    onClick: noop,
+    type: DropdownMenuItemType.BUTTON,
+  },
+  {
+    label: "Transactions",
+    type: DropdownMenuItemType.BUTTON,
+  },
+  {
+    type: DropdownMenuItemType.DIVIDER,
+  },
+  {
+    type: DropdownMenuItemType.BUTTON,
+    disabled: true,
+    label: "Dashboard",
+  },
+  {
+    type: DropdownMenuItemType.BUTTON,
+    disabled: true,
+    label: "Portfolio",
+  },
+  {
+    label: "Profile",
+    href: "/profile",
+  },
+  {
+    type: DropdownMenuItemType.EXTERNAL_LINK,
+    href: "https://pancakeswap.finance",
+    label: "Link",
+  },
+  {
+    type: DropdownMenuItemType.DIVIDER,
+  },
+  {
+    type: DropdownMenuItemType.BUTTON,
+    onClick: noop,
+    label: "Disconnect",
+  },
+];
+
+export const MENU_HEIGHT = 56;
+export const MENU_ENTRY_HEIGHT = 48;
+export const MOBILE_MENU_HEIGHT = 44;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
